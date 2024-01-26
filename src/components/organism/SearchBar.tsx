ef4ef52f1322751df66bba7e0ddcb25255db1059
Quentin PAYET <quentinpayet1203@gmail.com>
2024-01-26 10:20:12 +0400

    category name displayed in selectbox

--- src/components/organism/SearchBar.tsx
+++ src/components/organism/SearchBar.tsx
@@ -41,6 +41,47 @@ export function SearchBar(props: SearchBarPropsType): JSXElement {
     props.setImagesToDisplay(response.list_image_base64);
   }
 
+  enum CategoryNameEnum {
+    Meubles,
+    Electromenager,
+    Multimédia,
+    Exterieur,
+    Papeterie,
+    Telephone,
+    Informatique,
+    GrandeDistribution,
+    Bricolage,
+    Culture,
+    Sport,
+    Bebe,
+    BienEtre,
+    Vehicule,
+    Jouets,
+  }
+
+  // TODO: Put in utils AND CLEAN
+  function getCategoryName(idCategory: CategoryNameEnum) {
+    const dict_mapping = {
+      0: "Meubles",
+      1: "Électroménager",
+      2: "Multimédia",
+      3: "Exterieur",
+      4: "Papeterie",
+      5: "Téléphone",
+      6: "Informatique",
+      7: "Grande distribution",
+      8: "Bricolage",
+      9: "Culture",
+      10: "Mode",
+      11: "Sport",
+      12: "Bébé",
+      13: "Bien être",
+      14: "Véhicule",
+      15: "Jouets",
+    };
+
+    return dict_mapping[idCategory];
+  }
   return (
     <>
       {/* TODO: Put in css file */}
@@ -61,7 +102,9 @@ export function SearchBar(props: SearchBarPropsType): JSXElement {
             <option value={-1}>Veuillez selectionner une catégorie</option>
             <For each={categories()}>
               {/* TODO: Display category name */}
-              {(category) => <option value={category}>{category}</option>}
+              {(category) => (
+                <option value={category}>{getCategoryName(category)}</option>
+              )}
             </For>
           </select>
         </div>
